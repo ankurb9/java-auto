1789d106a50b1ade9afaee0e44cdda5801e8e52a
Ankur <ankurb9@gmail.com>
2024-01-15 00:07:05 +0530

    Initial commit

---
+++ src/main/java/org/playwright/Main.java
@@ -0,0 +1,22 @@
+package org.playwright;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.BrowserContext;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+            BrowserContext context = browser.newContext();
+            Page page = context.newPage();
+            page.navigate("http://playwright.dev");
+            context.close();
+
+        }
+    }
+}
